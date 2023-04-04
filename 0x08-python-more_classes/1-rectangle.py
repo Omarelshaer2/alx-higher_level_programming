43b6041ac5318e2dc980f919319c5b92f7f1a908
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-04-05 01:06:36 +0200

    dd

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,50 @@
-#!/usr/bin/python3
-"""
-Defines a class Rectangle
-"""
-
-
 class Rectangle:
-    """Representation of a rectangle"""
+    """
+    Represents a rectangle shape with a width and height.
+    """
     def __init__(self, width=0, height=0):
-        """Initializes the rectangle"""
-        self.height = height
-        self.width = width
+        """
+        Initializes the Rectangle object with optional width and height.
+        """
+        self._width = width
+        self._height = height
 
     @property
     def width(self):
-        """getter for the private instance attribute width"""
-        return self.__width
+        """
+        Retrieves the width of the Rectangle object.
+        """
+        return self._width
 
     @width.setter
     def width(self, value):
-        """setter for the private instance attribute width"""
+        """
+        Sets the width of the Rectangle object to the given value.
+        Raises a TypeError exception if value is not an integer.
+        Raises a ValueError exception if value is less than 0.
+        """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("Width must be an integer.")
         if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+            raise ValueError("Width must be >= 0.")
+        self._width = value
 
     @property
     def height(self):
-        """getter for the private instance attribute height"""
-        return self.__height
+        """
+        Retrieves the height of the Rectangle object.
+        """
+        return self._height
 
     @height.setter
     def height(self, value):
-        """setter for the private instance attribute height"""
+        """
+        Sets the height of the Rectangle object to the given value.
+        Raises a TypeError exception if value is not an integer.
+        Raises a ValueError exception if value is less than 0.
+        """
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("Height must be an integer.")
         if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+            raise ValueError("Height must be >= 0.")
+        self._height = value
