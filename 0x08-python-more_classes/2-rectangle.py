0d4b551661497526f3907139c549488ef63114d2
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-04-05 00:47:40 +0200

    x1

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,36 +1,43 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+"""
+
 class Rectangle:
+        """Represent a rectangle."""
+        
     def __init__(self, width=0, height=0):
-        self._width = 0
-        self._height = 0
         self.width = width
         self.height = height
 
-    def __repr__(self):
+    def __str__(self):
         return f"Rectangle({self.width}, {self.height})"
 
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        else:
+            self.__height = value
 
     def area(self):
         return self.width * self.height
@@ -38,4 +45,5 @@ class Rectangle:
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self.width + self.height)
+        else:
+            return 2 * (self.width + self.height)
