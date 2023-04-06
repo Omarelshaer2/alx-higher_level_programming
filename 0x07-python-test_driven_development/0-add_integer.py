d3a91783687b0c7fee3d5dfb7518937845c08fbf
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-04-06 17:29:16 +0200

    jj

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """
-This is the "0-add_integer" module.
-The 0-add_integer module supplies one function, add_integer(a, b).
+This module defines a function to add two numbers together, with input validation.
+
+The function add_integer(a, b) takes two arguments, a and b, and returns their sum.
+If either argument is not an integer or a float, a TypeError is raised. Floats are
+cast to integers before addition.
 """
 
 
 def add_integer(a, b):
-    """Return the addition of two numbers."""
+    """Returns the sum of two numbers a and b, after type conversion if necessary."""
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
+        raise TypeError("Input a must be a number")
     if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
+        raise TypeError("Input b must be a number")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
