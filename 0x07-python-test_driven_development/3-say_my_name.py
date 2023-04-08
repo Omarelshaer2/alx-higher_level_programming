f8a3198b5c66bba9a3b3c1af0a9e056eb08ce73a
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-04-08 00:49:34 +0000

    vv

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-This module defines the say_my_name function that can be used to print out a person's name.
+This module defines say_my_name function that can used to print a person's name
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints out the person's name, given their first name and (optional) last name."""
+    """Prints the person's name, given their first name and last name."""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
