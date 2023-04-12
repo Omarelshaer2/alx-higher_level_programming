01b1e9f160bab7355a6b1aad1830e4bb05379a89
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-04-12 04:59:58 +0200

    nnh

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class BaseGeometry and subclass Rectangle
+This module features a function called is_same_class
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
