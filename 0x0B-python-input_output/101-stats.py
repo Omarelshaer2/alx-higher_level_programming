8e552106cc686ce5be087d44cfcc0ab0320c8eec
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-04-14 02:42:13 +0200

    edit comments

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,51 +1,56 @@
 #!/usr/bin/python3
-"""Module containing a script that reads stdin line by line and computes,
-metrics
-Each 10 lines and after a keyboard interruption (CTRL + C), prints those,
-statistics since the beginning:
-Total file size: File size: <total size>
-where is the sum of all previous (see input format above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear, don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
-"""
-
 
 import sys
 
-file_size = 0
-status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-i = 0
+status_codes = {
+    '200': 0,  # Capture count of occurrences where HTTP 200 status received
+    '301': 0,  # Capture count of occurrences where HTTP 200 status received
+    '400': 0,  # Capture count of occurrences where HTTP 200 status received
+    '401': 0,  # Capture count of occurrences where HTTP 200 status received
+    '403': 0,  # Capture count of occurrences where HTTP 200 status received
+    '404': 0,  # Capture count of occurrences where HTTP 200 status received
+    '405': 0,  # Capture count of occurrences where HTTP 200 status received
+    '500': 0   # Capture count of occurrences where HTTP 200 status received
+}
+
+lc = 0  # Line counter
+file_size = 0  # Total file size counter
+
+
+def print_info():
+    """
+    Print file size and status code occurrences
+    """
+    print('File size: {:d}'.format(file_size))
+
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
+
+
 try:
     for line in sys.stdin:
-        tokens = line.split()
-        if len(tokens) >= 2:
-            a = i
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
-            try:
-                file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
-            except Exception:
-                if a == i:
-                    continue
-        if i % 10 == 0:
-            print("File size: {:d}".format(file_size))
-            for key, value in sorted(status_tally.items()):
-                if value:
-                    print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+        if lc != 0 and lc % 10 == 0:
+            print_info()
 
+        pieces = line.split()
+
+        try:
+            status = int(pieces[-2])
+
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+        except Exception:
+            pass
+
+        try:
+            file_size += int(pieces[-1])
+        except Exception:
+            pass
+
+        lc += 1
+
+    print_info()
 except KeyboardInterrupt:
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+    print_info()
+    raise
