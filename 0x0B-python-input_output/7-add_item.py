76d4daad01e306d75f8954857e53721ed8dd7e00
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-04-14 02:35:37 +0200

    editing comments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Module that loads, adds and saves arguments to a Python list"""
+"""Module for loading, adding, and saving arguments to a Python list as JSON"""
 from sys import argv
 
-
+# Importing the required functions to handle JSON file loading and saving
 load_file = __import__('6-load_from_json_file').load_from_json_file
 save_file = __import__('5-save_to_json_file').save_to_json_file
 
+# Attempt to load data from 'add_item.json'
 try:
     json_list = load_file('add_item.json')
 except (ValueError, FileNotFoundError):
     json_list = []
 
+# Add the command-line arguments to the existing list
 for item in argv[1:]:
     json_list.append(item)
 
+# Serialize the updated list as JSON and save it to 'add_item.json
 save_file(json_list, 'add_item.json')
