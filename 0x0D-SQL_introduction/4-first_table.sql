15ad5564495bca420d04d1a6e413ac0ce0514026
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-05-18 01:35:55 +0300

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,9 +1,9 @@
--- creates a table called first_table in the current database in your MySQL server.
--- The database name will be passed as an argument of the mysql command
+-- creating table called first_table in current database in my MySQL.
+-- database name will be passed as argument of mysql command
 -- first_table description:
 --             id INT
 --             name VARCHAR(256)
--- The database name will be passed as an argument of the mysql command
+-- The database name Will passed as argument of mysql command
 -- If the table first_table already exists, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
 
