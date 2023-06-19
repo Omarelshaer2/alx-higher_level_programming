b2293ca19d334b18db18e3be3ea9dd0195f0fb1c
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-06-19 05:15:39 +0300

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script that lists all State objects that contain the letter a from the
-database hbtn_0e_6_usa"""
+database"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
