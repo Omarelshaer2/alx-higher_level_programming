9b68744b8fe7e1a9a5e675a27f368614bbd3e57b
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-06-19 05:16:00 +0300

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Script that prints the first State object from the database
-hbtn_0e_6_usa"""
+"""Script that prints the first State object from the database"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
